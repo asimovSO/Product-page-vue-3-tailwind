407ffa1685f019eb0d6b082f68f8e1f3a13ce155
asimovSO <svoleg2002@gmail.com>
2025-11-05 20:21:21 +0200

    almost full footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer>
-    <div>
+  <footer class="flex justify-between items-center pt-12 mt-12">
+    <div class="subsciption_block mb-12">
       <img src="/img/logo.png" alt="Logo" class="my-8" />
       <div>
         <div class="flex border-b-[0.5px] border-gray-700 w-full">
@@ -14,12 +14,47 @@
           </button>
         </div>
         <div class="social-media flex gap-3 my-4">
-            <img src="/icons/facebook.svg" alt="Facebook" class="p-2 "/>
-            <img src="/icons/facebook.svg" alt="Facebook" class="p-2 "/>
-            <img src="/icons/facebook.svg" alt="Facebook" class="p-2 "/>
-            <img src="/icons/facebook.svg" alt="Facebook" class="p-2 "/>
+          <img src="/icons/facebook.svg" alt="Facebook" class="p-2" />
+          <img src="/icons/facebook.svg" alt="Facebook" class="p-2" />
+          <img src="/icons/facebook.svg" alt="Facebook" class="p-2" />
+          <img src="/icons/facebook.svg" alt="Facebook" class="p-2" />
         </div>
       </div>
     </div>
+    <div class="flex flex-wrap justify-items-start gap-14">
+      <div class="useful_links_col">
+        <h3 class="font-semibold mb-4">Shop</h3>
+        <ul class="flex flex-col gap-2">
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+          <li>FAQs</li>
+          <li>Terms & Conditions</li>
+        </ul>
+      </div>
+      <div class="useful_links_col">
+        <h3 class="font-semibold mb-4">Shop</h3>
+        <ul class="flex flex-col gap-2">
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+          <li>FAQs</li>
+          <li>Terms & Conditions</li>
+        </ul>
+      </div>
+      <div class="useful_links_col">
+        <h3 class="font-semibold mb-4">Shop</h3>
+        <ul class="flex flex-col gap-2">
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+          <li>FAQs</li>
+          <li>Terms & Conditions</li>
+        </ul>
+      </div>
+    </div>
   </footer>
+    <div class="border-b border-dashed border-slate-300 my-12"></div>
 </template>
+<script setup>
+</script>
